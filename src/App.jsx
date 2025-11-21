b40eb272215b733d5f9263bdd858c5ac772ad3a5
Suryaprakashrajendran <suryaprakashr1005@gmail.com>
2025-11-21 09:17:37 +0530

    all file

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const profile = {
   links: {
     github: "https://github.com/Suryaprakashrajendran?tab=repositories",
     linkedin: "https://www.linkedin.com/in/suryaprakash-r-/?originalSubdomain=in",
-    resume: "/src/images/suryaprakash_R.pdf",
+    resume: "/files/suryaprakash_R.pdf",
   },
   skills: [
     "Python",
